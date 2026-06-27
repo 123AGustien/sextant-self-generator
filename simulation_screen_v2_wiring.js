59008a942af5ebecf9c90570a89081fc90ca4516
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-06-27 07:37:24 +0700

    Add event listener for simulation screen load

---
+++ simulation_screen_v2_wiring.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+
+    const status = document.getElementById("status");
+
+    if (status) {
+        status.innerText = "ONLINE";
+    }
+
+    console.log("Sextant Simulation Screen v2 Wiring Loaded");
+
+});
